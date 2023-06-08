6aae445530bdb72133022fc5787593b12d02e087
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-06-08 13:07:38 +0200

    removed unused files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import axios from "axios";
-import Home from "./base/pages/home/home";
-import Header from "./base/pages/header/header";
-import Login from "./base/pages/login/login";
-import Register from "./base/pages/register/register";
-import CartPage from "./base/pages/cart/cart";
-import OrderPage from "./base/pages/order/order";
-import AddressPage from "./base/pages/address/adress";
-import PaymentMethod from "./base/pages/payMethod/payMethod";
-import PlaceOrder from "./base/pages/placeOrder/placeOrder";
-import OrderHistory from "./base/pages/orderHistory/orderHistory";
-import ProfilePage from "./base/pages/profile/profile";
-import ProtectedRoute from "./base/components/protectedRoute/protectedRoute";
-import AdminRoute from "./base/components/adminRoute/adminRoute";
-import UserListPage from "./base/pages/userList/userList";
-import UserEditPage from "./base/pages/editUser/editUser";
-import Auction from "./base/pages/aution/aution";
-import CreateAuction from "./base/pages/createAuction/createdAution";
-import AuctionDetail from "./base/pages/auctionItems/autionItems";
+import Home from "./base/pages/home";
+import Header from "./base/pages/header";
+import Login from "./base/pages/login";
+import Register from "./base/pages/register";
+import CartPage from "./base/pages/cart";
+import OrderPage from "./base/pages/order";
+import Address from "./base/pages/adress";
+import PaymentMethod from "./base/pages/payMethod";
+import PlaceOrder from "./base/pages/placeOrder";
+import OrderHistory from "./base/pages/orderHistory";
+import ProfilePage from "./base/pages/profile";
+import ProtectedRoute from "./base/components/protectedRoute";
+import AdminRoute from "./base/components/adminRoute";
+import UserListPage from "./base/pages/userList";
+import UserEditPage from "./base/pages/editUser";
+import AuctionPage from "./base/pages/aution";
+import CreateAuction from "./base/pages/createdAution";
+import AuctionDetail from "./base/pages/autionDetail";
 
 // Set the base URL for all axios requests
 axios.defaults.baseURL =
@@ -55,7 +55,7 @@ function App() {
           element={
             <>
               <Header />
-              <AddressPage />
+              <Address />
             </>
           }
         />
@@ -146,7 +146,7 @@ function App() {
           element={
             <>
               <Header />
-              <Auction />
+              <AuctionPage />
             </>
           }
         />
